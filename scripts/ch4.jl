d65d7a25d95016f2cc68922e4e937083a9c22b44
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2024-03-05 13:57:18 +0000

    corrections

--- scripts/ch4.jl
+++ scripts/ch4.jl
@@ -58,7 +58,7 @@ std_t43_birdspertow = std(t43_birds, fweights(t43_captures))
 function p_c(c, k, m)
     f1 = gamma(k + c) / (gamma(k) * factorial(c))
     f2 = (k / (k + m))^k 
-    f3 = (m = (m + k))^c 
+    f3 = (m / (m + k))^c 
     return f1 * f2 * f3 
 end
 
@@ -103,17 +103,7 @@ fig4_1 = let
             sum(_predictedcatch[9:end]) 
         for i ∈ eachindex(_birds) 
     ]  # these are already probabilities so do not need to divide by the total
-    _predictedcapturesfrequencydifference = _capturesfrequency .- _predictedcatchagg
-
-    # use Distributions.NegativeBinomial
-    _predictedcatch1 = [ pdf(NegativeBinomial(t43_k, t43_m), x) for x in t43_birds ]
-    _predictedcatchagg1 = [ 
-        i <= 8 ? 
-        _predictedcatch1[i] : 
-            sum(_predictedcatch1[9:end]) 
-        for i ∈ eachindex(_birds) 
-    ] 
-    _predictedcapturesfrequencydifference1 = _capturesfrequency .- _predictedcatchagg1
+    _predictedcapturesfrequencydifference = _predictedcatchagg .- _capturesfrequency
 
     # used Distributions.Poisson
     _predictedcatch2 = [ pdf(Poisson(t43_m), x) for x in t43_birds ]
@@ -123,29 +113,28 @@ fig4_1 = let
             sum(_predictedcatch2[9:end]) 
         for i ∈ eachindex(_birds) 
     ] 
-    _predictedcapturesfrequencydifference2 = _capturesfrequency .- _predictedcatchagg2
+    _predictedcapturesfrequencydifference2 = _predictedcatchagg2 .- _capturesfrequency
 
     fig = Figure()
-    axs = [ Axis(fig[i, j]) for i in 1:2, j in 1:4 ]
+    axs = [ Axis(fig[i, j]) for i in 1:2, j in 1:3 ]
     barplot!(axs[1, 1], _birds, _capturesfrequency)
     delete!(axs[2, 1])
     barplot!(axs[1, 2], _birds, _predictedcatchagg)
     scatter!(axs[2, 2], _birds, _predictedcapturesfrequencydifference)
-    barplot!(axs[1, 3], _birds, _predictedcatchagg1)
-    scatter!(axs[2, 3], _birds, _predictedcapturesfrequencydifference1)
-    barplot!(axs[1, 4], _birds, _predictedcatchagg2)
-    scatter!(axs[2, 4], _birds, _predictedcapturesfrequencydifference2)
+    barplot!(axs[1, 3], _birds, _predictedcatchagg2)
+    scatter!(axs[2, 3], _birds, _predictedcapturesfrequencydifference2)
 
     linkaxes!(axs[1, :]...)
-    linkaxes!(axs[2, 2:3]...)
 
     Label(fig.layout[0, 1], "Observed data"; fontsize=11.84, halign=:left, tellwidth=false)    
     Label(fig.layout[0, 2], "Predicted data"; fontsize=11.84, halign=:left, tellwidth=false)    
-    Label(fig.layout[0, 3], "Distributions.NB"; fontsize=11.84, halign=:left, tellwidth=false)    
-    Label(fig.layout[0, 4], "Poisson"; fontsize=11.84, halign=:left, tellwidth=false)    
+    Label(fig.layout[0, 3], "Poisson"; fontsize=11.84, halign=:left, tellwidth=false)    
     Label(fig.layout[1, 0], "Frequency"; fontsize=11.84, rotation=π/2, tellheight=false)
-    Label(fig.layout[2, 0], "Residual"; fontsize=11.84, rotation=π/2, tellheight=false)
-    Label(fig.layout[3, 1:4], "Albatrosses caught"; fontsize=11.84, tellwidth=false)    
+    Label(
+        fig.layout[2, 1], "Residual"; 
+        fontsize=11.84, halign=:right, rotation=π/2, tellheight=false, tellwidth=false
+    )
+    Label(fig.layout[3, 1:3], "Albatrosses caught"; fontsize=11.84, tellwidth=false)    
 
     fig
 end
@@ -155,16 +144,17 @@ end
 # Equation 4.9
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# use predicted values rather than proportions 
 modelchisq = sum(
     [ 
-        (t43_captures[i] / sum(t43_captures) - p_c(t43_birds[i], t43_k, t43_m))^2 / 
-        p_c(t43_birds[i], t43_k, t43_m)
+        (t43_captures[i] - p_c(t43_birds[i], t43_k, t43_m) * sum(t43_captures))^2 / 
+        (p_c(t43_birds[i], t43_k, t43_m) * sum(t43_captures))
         for i in eachindex(t43_birds)
     ]
 )
-#31012.67075500003
-cdf(Chisq(17), modelchisq)
-#1.0
+#22.04171977027245
+1 - cdf(Chisq(17), modelchisq)
+#0.18311991466364663
 
 # note that `p_c(t43_birds[i], t43_k, t43_m)` becomes very small but is never 0 so function
 # can run without grouping or omitting values (both of which were necessary in the text) but
@@ -185,81 +175,10 @@ function probabilities_C_ij(k, m; cmax=17)
     return probC_ij_equals_x
 end
 
-probabilities_C_ij(t43_k, t43_m)
-#19-element Vector{Float64}:
-# 0.9009824108996544
-# 0.018477827816600872
-# 0.0033230674386298177
-# 0.0007741137534502386
-# 0.00020088789260389956
-# 5.533259505088145e-5
-# 1.5828570527847962e-5
-# 4.648052950494178e-6
-# 1.3913482149889877e-6
-# 4.226441512058045e-7
-# 1.298813469409691e-7
-# 4.0289657971204886e-8
-# 1.2595260167773849e-8
-# 3.963229859705166e-9
-# 1.2540108092503316e-9
-# 3.9868612007307404e-10
-# 1.2728315269843017e-10
-# 4.0785099351581445e-11
-# 0.07616388043790523
-
-# note that in this set of probabilities, the greatest value of catches is the second most
-# probable outcome. Therefore, instead use Distributions.NegativeBinomial
-
-probabilities_C_ij2(k, m; cmax=17) = [ pdf(NegativeBinomial(k, m), x) for x in 0:(cmax + 1) ]
-
-probabilities_C_ij2(t43_k, t43_m)
-#19-element Vector{Float64}:
-# 0.925657104818187
-# 0.04037152989019001
-# 0.015440235210529297
-# 0.007649094137715043
-# 0.004221331422776154
-# 0.0024726758762583755
-# 0.0015042440909430058
-# 0.0009393731675533664
-# 0.0005979892605346502
-# 0.00038629837208472363
-# 0.0002524557483250107
-# 0.00016654152072651494
-# 0.00011072012145291256
-# 7.408992198550854e-5
-# 4.9854185072517194e-5
-# 3.3707141429221146e-5
-# 2.288506742039017e-5
-# 1.5594564906000643e-5
-# 1.0661136194750375e-5
-
 # also equip the function to use a Poisson distribution 
 probabilities_C_ij_pois(k, m; cmax=17) = [ pdf(Poisson(m), x) for x in 0:(cmax + 1) ]
 
-probabilities_C_ij_pois(t43_k, t43_m)
-#19-element Vector{Float64}:
-# 0.7567617528988304
-# 0.21091464685028719
-# 0.029391673195413477
-# 0.0027305530653487075
-# 0.00019025592707279187
-# 1.0605124140066432e-5
-# 4.926200362349704e-7
-# 1.961379344780096e-8
-# 6.833122020554963e-10
-# 2.1160417504505646e-11
-# 5.897552258780825e-13
-# 1.4942617459158935e-14
-# 3.4705075852747343e-16
-# 7.440415884732778e-18
-# 1.4812103608721016e-19
-# 2.7521560031068557e-21
-# 4.7940287122122907e-23
-# 7.859578844862516e-25
-# 1.2169544848356363e-26
-
-function sample_C_ij(k, m; cmax=17, probfunction=probabilities_C_ij2)
+function sample_C_ij(k, m; cmax=17, probfunction=probabilities_C_ij)
     probC_ij_equals_x = probfunction(k, m; cmax)
     predC_ij = sample(0:(cmax + 1), pweights(probC_ij_equals_x))
     return predC_ij
@@ -336,7 +255,7 @@ end
 
 # so plan to do fewer runs at greater numbers 
 
-ntowvec = [ collect(1:1:99); collect(100:10:990); collect(1000:100:5000) ]
+ntowvec = [ collect(1:1:99); collect(100:10:5000) ]
 
 seed!(1)
 chanceofsuccess = [ 
@@ -375,6 +294,9 @@ let
     ax2 = Axis(fig4_2[2, 1])
     lines!(ax2, ntowvec, chanceofsuccesspois)
     vlines!(ax2, sum(t43_captures); color=:black, linestyle=:dot)
+
+    Label(fig4_2.layout[1:2, 0], "Chance of success"; fontsize=11.84, rotation=π/2, tellheight=false)
+    Label(fig4_2.layout[3, 1], "Number of tows observed"; fontsize=11.84, tellwidth=false) 
 end
 
 fig4_2
